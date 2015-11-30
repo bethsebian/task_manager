74ec8cdebf6dd3aa7a3bc11e95f584b86ea129a0
Beth Sebian <esebian@gmail.com>
2015-11-29 19:43:18 -0700

    finish task manager markdown instructions

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,7 +1,29 @@
+require 'models/task_manager'
+
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
   get '/' do
-    'hello, world!'
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = TaskManager.all
+    erb :index
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+    TaskManager.create(params[:task])
+    redirect '/tasks'
+    # "<p>Params: #{params}</p> <p>Task params: #{params[:task]}</p>"
+  end
+
+  get '/tasks/:id' do |id|
+    @task = TaskManager.find(id.to_i)
+    erb :show
   end
 end
